43e4ef33f6adddb2668f315162cb5e65e732ff97
Daniel Palmer <daniel@0x0f.com>
2023-02-02 18:01:17 +0900

    Add setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='isptool',
+    version='0',
+    scripts=['isptool'],
+    url='',
+    license='GPLv3',
+    author='Daniel Palmer',
+    author_email='daniel@thingy.jp',
+    description=''
+)
